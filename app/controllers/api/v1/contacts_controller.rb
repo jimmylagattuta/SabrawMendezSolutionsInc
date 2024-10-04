2bda4a91a53d1f42a3934d785fbf01b0dbb1ca09
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-10-04 09:40:22 -0700

    big update

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,16 +1,16 @@
 module Api
     module V1
-      class ContactsController < ApplicationController  
+      class ContactsController < ApplicationController
         def create
-            contact_params = params.require(:contact).permit(:first_name, :last_name, :email, :message)
-            logger.info("Attempting to send email with params: #{contact_params.inspect}")
-            ContactMailer.contact_email(contact_params).deliver_now
-            logger.info("Email sent successfully")
-            render json: { success: true, message: 'Email sent successfully' }, status: :ok
-          rescue StandardError => e
-            logger.error("Error sending email: #{e.message}")
-            render json: { success: false, message: 'Error sending email', error: e.message }, status: :unprocessable_entity
-          end          
+          contact_params = params.require(:contact).permit(:first_name, :last_name, :email, :message)
+          puts "Attempting to send email with params: #{contact_params.inspect}"
+          ContactMailer.contact_email(contact_params).deliver_now
+          puts "Email sent successfully"
+          render json: { success: true, message: 'Email sent successfully' }, status: :ok
+        rescue StandardError => e
+          puts "Error sending email: #{e.message}"
+          render json: { success: false, message: 'Error sending email', error: e.message }, status: :unprocessable_entity
+        end
       end
     end
   end

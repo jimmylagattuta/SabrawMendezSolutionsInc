ef7222b04d2af65b288542d8032d0c12b6af044f
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-10-04 09:21:01 -0700

    big update

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete "/logout", to: "sessions#destroy"  
   delete "/logout", to: "sessions#destroy"
-  post '/contact', to: 'api/v1/contacts#create'
+  post '/contact', to: 'api/v1/contact#create'
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
